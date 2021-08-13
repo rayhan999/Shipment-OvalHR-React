2b21112bc183f8e09e4a22bd5154946747b5763d
rayhan999 <58140227+rayhan999@users.noreply.github.com>
2021-08-13 15:59:59 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { createContext, lazy, Suspense, useEffect, useState } from "react";
+import 'bootstrap/dist/css/bootstrap.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from "react-router-dom";
+import Home from "./Pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <Router>
+        {/* <Toaster /> */}
+        <Suspense 
+        // fallback={<LoadingSpinner />}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            {/* <PrivateRoute path="/dashboard/:panel">
+              <Dashboard adminLoading={adminLoading} />
+            </PrivateRoute> */}
+          </Switch>
+        </Suspense>
+      </Router>
+    </>
   );
 }
 
---
+++ src/Pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { MDBDataTableV5 } from 'mdbreact';
+import Navbar from '../components/Navbar/Navbar';
+import DataTable from '../components/DataTable/DataTable';
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <div className="container">
+                <DataTable></DataTable>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
